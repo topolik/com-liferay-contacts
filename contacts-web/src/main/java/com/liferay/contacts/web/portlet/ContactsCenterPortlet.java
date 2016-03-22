brianchandotcom/liferay-portal

LPS-64537 update icon name - contacts-web

modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/portlet/ContactsCenterPortlet.java
@@ -127,7 +127,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.friendly-url-mapping=contacts",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contact.png",
 		"javax.portlet.display-name=Contacts Center",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.info.keywords=Contacts Center",

modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/portlet/MembersPortlet.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.friendly-url-mapping=contacts",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contact.png",
 		"javax.portlet.display-name=Members",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.info.keywords=Members",

modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/portlet/MyContactsPortlet.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.css-class-wrapper=contacts-portlet",
 		"com.liferay.portlet.display-category=category.social",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contact.png",
 		"javax.portlet.display-name=My Contacts",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.info.keywords=My Contacts",

modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/portlet/ProfilePortlet.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.css-class-wrapper=contacts-portlet",
 		"com.liferay.portlet.display-category=category.social",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contact.png",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=Profile",
 		"javax.portlet.expiration-cache=0",

topolik/com-liferay-contacts

contacts-web/src/main/java/com/liferay/contacts/web/portlet/ContactsCenterPortlet.java
@@ -127,7 +127,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.friendly-url-mapping=contacts",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contacts_center.png",
 		"javax.portlet.display-name=Contacts Center",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.info.keywords=Contacts Center",

contacts-web/src/main/java/com/liferay/contacts/web/portlet/MembersPortlet.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.friendly-url-mapping=contacts",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contacts_center.png",
 		"javax.portlet.display-name=Members",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.info.keywords=Members",

contacts-web/src/main/java/com/liferay/contacts/web/portlet/MyContactsPortlet.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.css-class-wrapper=contacts-portlet",
 		"com.liferay.portlet.display-category=category.social",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contacts_center.png",
 		"javax.portlet.display-name=My Contacts",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.info.keywords=My Contacts",

contacts-web/src/main/java/com/liferay/contacts/web/portlet/ProfilePortlet.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.css-class-wrapper=contacts-portlet",
 		"com.liferay.portlet.display-category=category.social",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.icon=/icon.png",
+		"com.liferay.portlet.icon=/icons/contacts_center.png",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=Profile",
 		"javax.portlet.expiration-cache=0",